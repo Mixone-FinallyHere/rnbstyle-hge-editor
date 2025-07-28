e45dd3299a99bd1392221971d7098892c74bb57e
Miguel <miguel.terol.93@gmail.com>
2025-07-28 16:19:14 +0200

    Ignores comments when using s_parse
    Prolly same method could be used in other places

--- hgecsv/s_parser.py
+++ hgecsv/s_parser.py
@@ -16,7 +16,7 @@ def parse_s(rows):
 
         if len(parts) > 1:
             key = parts[0]
-            value = parts[1]
+            value = parts[1].split('//')[0].strip()
 
             match key:
                 case "trainerdata":
